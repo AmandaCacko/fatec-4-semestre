bca10ac394f83b9d70952390cc74dbda6392b2df
Sabrina Onishi <sabrina.m.onishi@hotmail.com>
2024-05-16 19:37:37 -0300

    Criação da página Atribuir Etapas

---
+++ src/pages/etapas/index.tsx
@@ -0,0 +1,14 @@
+import Layout from "../../components/layout"
+
+//<img src=""/>
+
+function Etapas() {
+    return (
+        <Layout>
+           
+      
+        </Layout>
+    )
+}
+
+export default Etapas
--- src/routes.tsx
+++ src/routes.tsx
@@ -5,13 +5,15 @@ import NotFound from './pages/404'
 import Tarefas from './pages/tarefas'
 import Formulario from './pages/formulario'
 import Codigo from './pages/codigo'
+import Etapas from './pages/etapas'
 
 const router = createBrowserRouter([
   {path: '/', element: <Home />, errorElement: <NotFound />},
   {path: '/sobre', element: <Sobre />},
   {path: '/tarefas', element: <Tarefas />},
   {path: '/formulario', element: <Formulario />},
-  {path: '/codigo', element: <Codigo />}
+  {path: '/codigo', element: <Codigo />},
+  {path: '/etapas', element: <Etapas />}
 ])
 
 export default router

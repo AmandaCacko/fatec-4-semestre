8328a1ee7e0b16aabb962d0d1981de01d373201f
Sabrina Onishi <sabrina.m.onishi@hotmail.com>
2024-05-17 01:13:46 -0300

    Atribuir etapas - Pop-ups da Busca por nome ou código

--- src/components/etapa2/index.tsx
+++ src/components/etapa2/index.tsx
@@ -8,29 +8,37 @@ import {
     IconButton,
     Input,
     InputGroup,
-    NumberInput,
-    NumberInputField,
-    NumberInputStepper,
-    NumberIncrementStepper,
-    NumberDecrementStepper,
-    InputLeftElement,
-    Link,
     Stack,
-    Text,
-    Textarea,
-    Tooltip,
-    useClipboard,
     useColorModeValue,
     VStack,
     Checkbox,
+    AlertDialog,
+    AlertDialogBody,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogContent,
+    AlertDialogOverlay,
+    AlertDialogCloseButton,
+    Spacer,
+    useDisclosure,
 
 } from '@chakra-ui/react';
 import { ArrowBackIcon } from '@chakra-ui/icons'
-
+import React from 'react';
 
 function InserirCodigo() {
 
-    
+    const {
+        isOpen: isOpenBuscar,
+        onOpen: onOpenBuscar,
+        onClose: onCloseBuscar,
+      } = useDisclosure();
+      const {
+        isOpen: isOpenAtribuir,
+        onOpen: onOpenAtribuir,
+        onClose: onCloseAtribuir,
+      } = useDisclosure();
+    const cancelRef = React.useRef<HTMLButtonElement>(null);
     
     return (
         <Flex padding={100}
@@ -75,9 +83,32 @@ function InserirCodigo() {
                                 transform: 'translateY(-2px)',
                                 boxShadow: 'lg',
                             }}
-                            marginTop='30px'>
+                            marginTop='30px'
+                            onClick={onOpenBuscar}>
                             Buscar
                         </Button>
+                        <AlertDialog
+                            motionPreset='slideInBottom'
+                            leastDestructiveRef={cancelRef}
+                            onClose={onCloseBuscar}
+                            isOpen={isOpenBuscar}
+                            isCentered
+                        >
+                            <AlertDialogOverlay />
+
+                            <AlertDialogContent>
+                            <AlertDialogHeader>Código não encontrado</AlertDialogHeader>
+                            <AlertDialogCloseButton />
+                            <AlertDialogBody>
+                                O código XXXXXXX não existe
+                            </AlertDialogBody>
+                            <AlertDialogFooter>
+                            <Button ref={cancelRef} onClick={onCloseBuscar}>
+                            Criar código
+                            </Button>
+                            </AlertDialogFooter>
+                            </AlertDialogContent>
+                        </AlertDialog>
                 </Flex>
 
                 <Flex flexDirection={'row'} padding={'50px'} width={'850px'} height={'500px'} >
@@ -140,9 +171,35 @@ function InserirCodigo() {
                             _hover={{
                                 transform: 'translateY(-2px)',
                                 boxShadow: 'lg',
-                            }}>
+                            }}
+                            onClick={onOpenAtribuir}>
                             Atualizar
                         </Button>
+                        <AlertDialog
+                            motionPreset='slideInBottom'
+                            leastDestructiveRef={cancelRef}
+                            onClose={onCloseAtribuir}
+                            isOpen={isOpenAtribuir}
+                            isCentered
+                        >
+                            <AlertDialogOverlay />
+
+                            <AlertDialogContent>
+                            <AlertDialogHeader>Tarefa atribuída com sucesso!</AlertDialogHeader>
+                            <AlertDialogCloseButton />
+                            <AlertDialogBody>
+                                Nome do Funcionário - XXXXXXX
+                                <Spacer />
+                                Formulários Atribuidos:
+                                <Spacer />
+                                    • Centro de Distribuição - Dentro do caminhão
+                                <Spacer />
+                                    • Centro de Distribuição - Fora do caminhão
+                            </AlertDialogBody>
+                            <AlertDialogFooter>
+                            </AlertDialogFooter>
+                            </AlertDialogContent>
+                        </AlertDialog>
                 </VStack>
             </Box>
         </Flex>

fc1527122cb5437fcfd726e5c1522bf6708add1a
amandacacko <amandacacko@gmail.com>
2024-05-26 21:42:11 -0300

    Base url corrigida

--- src/helpers/axios.ts
+++ src/helpers/axios.ts
@@ -1,8 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    // baseURL: 'https://exercicio-api.onrender.com',
-    baseURL: 'http://localhost:3001',
+    baseURL: 'https://exercicio-api.onrender.com',
     headers: {
         'Content-Type': 'application/json'
     }
